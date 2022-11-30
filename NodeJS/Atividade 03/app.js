ce672ae98dfce625f6ec14f478e7bad1e1980a86
aluno <aluno0@unicap.br>
2022-11-30 19:43:09 -0300

    Atualizações

--- NodeJS/Atividade 03/app.js
+++ NodeJS/Atividade 03/app.js
@@ -1,11 +1,19 @@
 const express = require('express');
+const mysql = require('mysql2');
+const connect = require('database/connect');
 
 const app = express();
 
+app.set('view engine', 'ejs');
+
 app.get('/',(Request, Response)=>{
-    Response.send('Esse é um simples Node'
-    + ' App Rodando no servidor')
+    Response.render('pages/index')
 });
+
+app.get('/about',(Request, Response)=>{
+    Response.render('pages/index')
+});
+
 app.post('/',(Request, Response)=>{
 
 });
@@ -19,6 +27,6 @@ app.delete('/',(Request, Response)=>{
 });
 
 
-const PORT = process.env.PORT||5000
+const PORT = 8080
 
-app.listen(PORT, console.log(`Server started on port ${PORT}`));
+app.listen(PORT,() => {console.log('App Rodando!')});
