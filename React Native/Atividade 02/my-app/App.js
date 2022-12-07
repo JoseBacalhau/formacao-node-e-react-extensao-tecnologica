a96356d4eb588da5f37f0183ba51fe9928818a3c
Vitor <jvmbacalhau@gmail.com>
2022-12-07 16:55:41 -0300

    Atualização

--- React Native/Atividade 02/my-app/App.js
+++ React Native/Atividade 02/my-app/App.js
@@ -1,11 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { Button } from 'react-native-elements';
+
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Meu APP</Text>
+      <Button title={'Botão'}/>
     </View>
   );
 }
