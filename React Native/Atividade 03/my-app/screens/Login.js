995fb282b96db2801e5740ec630f4a76b0e84eeb
Vitor <jvmbacalhau@gmail.com>
2022-12-10 23:53:40 -0300

    Atividade 03

--- React Native/Atividade 03/my-app/screens/Login.js
+++ React Native/Atividade 03/my-app/screens/Login.js
@@ -1,18 +1,45 @@
-import * as React from "react";
+import React, { useState, useEffect } from "react";
 import { View, Text } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { Button } from "react-native-elements";
+import { Button, Input, Avatar} from "react-native-elements";
 import Icon from "react-native-vector-icons/FontAwesome";
 
-export default function LoginScreen({navigation}) {
+export default function LoginScreen({ navigation }) {
   return (
     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text>Home Screen</Text>
+      <Avatar
+          size={"large"}
+          rounded
+          source={{
+            uri: "https://cdn-icons-png.flaticon.com/512/1144/1144760.png",
+          }}
+        />
+      <Text style={{paddingTop: 60}}>Login</Text>
+      <Input style={{ borderColor: "black", borderWidth:1, alignItems: "center"}}></Input>
+      <Text style={{paddingTop: 10}}>Senha</Text>
+      <Input style={{ borderColor: "black", borderWidth: 1,  alignItems: "center",   }}></Input>
       <Button
-      onPress={()=> navigation.navigate('Cadastro')}
-      title="Tela Inicial" />
+        title={"Login"}
+        buttonStyle={{ backgroundColor: "rgba(78, 116, 289, 1)" }}
+        containerStyle={{
+          height: 40,
+          width: 200,
+          marginHorizontal: 50,
+          marginVertical: 10,
+        }}
+      />
+      <Button
+        title={"Cadastro"}
+        onPress={() => navigation.navigate("Cadastro")}
+        buttonStyle={{ backgroundColor: "rgba(214, 61, 57, 1)" }}
+        containerStyle={{
+          height: 40,
+          width: 200,
+          marginHorizontal: 50,
+          marginVertical: 10,
+        }}
+      />
     </View>
-    
   );
 }
--- React Native/Atividade 04/Login1.js
+++ React Native/Atividade 04/Login1.js
@@ -5,23 +5,13 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { Button, Input } from "react-native-elements";
 import Icon from "react-native-vector-icons/FontAwesome";
 
+
 export default function CadastroScreen({ navigation }) {
+  
   return (
     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text>Email</Text>
-      <Input style={{ borderColor: "black", borderWidth: 1 }}></Input>
-      <Text>Senha</Text>
-      <Input style={{ borderColor: "black", borderWidth: 1 }}></Input>
-      <Button
-        title={"Salvar"}
-        onPress={() => navigation.navigate("Login")}
-        buttonStyle={{ backgroundColor: "rgba(78, 116, 289, 1)" }}
-        containerStyle={{
-          height: 40,
-          width: 200,
-          marginHorizontal: 50,
-          marginVertical: 10,
-        }}
+      <Button title="Tela de Cadastro" 
+      onPress={() => navigation.navigate('Cadastro')} 
       />
     </View>
   );
